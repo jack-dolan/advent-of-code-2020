baaf6c887438b1b89568a61aaf3cce877560de4a
Jack Dolan <jack@dolanjack.com>
2020-12-11 08:16:30 -0500

    Going to refactor to pass the arrangment SO FAR

--- 10/adapters.py
+++ 10/adapters.py
@@ -1,3 +1,4 @@
+# from functools import lru_cache
 
 def joltage_difference_counts(ordered_output_list):
     difference_counts = {
@@ -17,6 +18,7 @@ def joltage_difference_counts(ordered_output_list):
             difference_counts[joltage_difference] += 1
     return(difference_counts)
 
+# @lru_cache(maxsize=None)
 def get_all_valid_arrangements(available_adapter_list, starting_output_joltage, goal_input_joltage):
     sum_of_valid_arrangements = 0
     for adapter in available_adapter_list:
@@ -27,18 +29,18 @@ def get_all_valid_arrangements(available_adapter_list, starting_output_joltage, 
             if(adapter + 3 >= goal_input_joltage):
                 sum_of_valid_arrangements += 1
                 # print("Success - ", starting_output_joltage, " -> ", adapter, " works!")
-            list_less_adapter = available_adapter_list[:]
+            list_less_adapter = list(available_adapter_list)[:]
             list_less_adapter.remove(adapter)
             if (len(list_less_adapter) > 0):
-                sum_of_valid_arrangements += get_all_valid_arrangements(list_less_adapter, adapter, goal_input_joltage)
+                sum_of_valid_arrangements += get_all_valid_arrangements(tuple(list_less_adapter), adapter, goal_input_joltage)
     return sum_of_valid_arrangements
 
 
 
-input = open('./input.txt')  # Solution!
+# input = open('./input.txt')  # Solution!
 # input = open('./input_sample.txt')  # 8
 # input = open('./input_sample_small.txt')  # 4
-# input = open('./input_sample_large.txt')  # 19208
+input = open('./input_sample_large.txt')  # 19208
 lines = input.readlines()
 lines_ints = list(map(int, lines))
 lines_ints.sort()
@@ -47,4 +49,4 @@ device_adapter_rating = lines_ints[-1] + 3
 
 part1_counts = joltage_difference_counts(lines_ints)
 print("Part 1 - The joltage difference product is: ", part1_counts[1] * part1_counts[3])
-print("Part 2 - The total # of valid arrangements is: ", get_all_valid_arrangements(lines_ints, 0, device_adapter_rating))
+print("Part 2 - The total # of valid arrangements is: ", get_all_valid_arrangements(tuple(lines_ints), 0, device_adapter_rating))
